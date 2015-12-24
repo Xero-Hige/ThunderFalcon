8f41e99c1a625b0ae8217c9ea16f8e9d51572ced
Xero-Hige <gaston.martinez.90@gmail.com>
2015-12-23 23:57:36 -0300

    aasdasd

--- topologies/wordcount.clj
+++ topologies/wordcount.clj
@@ -22,8 +22,8 @@
 
     "countz-bolt" (python-bolt-spec
           options
-          {""count-bolt" :shuffle}
-          "bolts.wordcount.WorldCounter"
+          {"count-bolt" :shuffle}
+          "bolts.worldcount.WorldCounter"
           ["word" "count"]
           :p 2
           )
