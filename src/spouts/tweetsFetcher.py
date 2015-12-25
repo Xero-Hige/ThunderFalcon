ce06cabb4a3f1e805d2152d05faf895c35b73612
Xero-Hige <gaston.martinez.90@gmail.com>
2015-12-25 18:27:10 -0300

    Added debug info (Spout not launching)

--- src/spouts/tweetsFetcher.py
+++ src/spouts/tweetsFetcher.py
@@ -15,29 +15,41 @@ class TweetSpout(Spout):
         self.TOKEN_SECRET = ""
 
         self.get_keys()
+        self.log("\n\n Got keys \n\n")
 
         self.auth = OAuth(self.TOKEN_KEY, self.TOKEN_SECRET, self.CONSUMER_KEY, self.CONSUMER_SECRET)
+        self.log("\n\n Got Oauth \n\n")
 
         self.generate_tweet_pool()
 
     def generate_tweet_pool(self):
         twitter_stream = TwitterStream(auth=self.auth)
+        self.log("\n\n Got Stream \n\n")
+
         self.tweets = twitter_stream.statuses.sample()
+        self.log("\n\n Got Iterator \n\n")
 
     def get_keys(self):
         with open("keys.rsa", "r") as f:
             self.CONSUMER_KEY = f.readline()
+            self.log("\n\n Key: %s \n\n" % (self.CONSUMER_KEY))
+
             self.CONSUMER_SECRET = f.readline()
+            self.log("\n\n Key: %s \n\n" % (self.CONSUMER_SECRET))
+
             self.TOKEN_KEY = f.readline()
+            self.log("\n\n Key: %s \n\n" % (self.TOKEN_KEY))
+
             self.TOKEN_SECRET = f.readline()
+            self.log("\n\n Key: %s \n\n" % (self.TOKEN_SECRET))
 
     def next_tuple(self):
-        tweet_to_emit = None
-        while not tweet_to_emit:
+        while True:
             try:
                 tweet_to_emit = self.tweets.next()
+                self.emit([tweet_to_emit])
+                break
+
             except Exception, e:
                 self.log("Error: %s" % (e.message))
                 self.generate_tweet_pool()
-
-        self.emit([tweet_to_emit])
